cde63f43792b462c0ca57d94dc7f3c311680dab9
rahneverd <rahneverd@gmail.com>
2022-12-21 14:40:04 +0500

    app structured

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ReipeListComponent } from './recipes/reipe-list/reipe-list.component';
+import { ReipeDetailComponent } from './recipes/reipe-detail/reipe-detail.component';
+import { ReipeItemComponent } from './recipes/recipe-list/reipe-item/reipe-item.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    ReipeListComponent,
+    ReipeDetailComponent,
+    ReipeItemComponent,
+    RecipeDetailComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
